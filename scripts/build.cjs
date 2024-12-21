5cd606fa3221341d0aba293d00bd1123fd042511
Pascal LLEONART <pascal.lleonart@laposte.net>
2024-12-21 10:41:24 +0100

    fix(build): fix deps build

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,18 +1,4 @@
-const esbuild = require("esbuild")
-const { existsSync, rmSync } = require("node:fs")
-const pkgjson = require("../package.json")
+const build = require("@fehujs/build")
+const pkgJson = require("../package.json")
 
-if (existsSync('build'))
-    rmSync('build', { recursive: true })
-
-esbuild.build({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    outfile: './build/index.js',
-    platform: 'node',
-    target: 'esnext',
-    format: 'esm',
-    sourcemap: true,
-    tsconfig: './tsconfig.json',
-    external: pkgjson.dependencies ? Object.keys(pkgjson.dependencies) : [],
-}).catch(() => process.exit(1))
+build(pkgJson.dependencies)
